e30d1f999e00eeda0d86da8dcb8cf5788e450eff
Oscar <osmithsi@sfu.ca>
2018-09-27 23:36:40 -0700

    Updated aliases

--- files/aliases.zsh
+++ files/aliases.zsh
@@ -6,15 +6,18 @@ alias zshrc="nano ~/.zshrc"
 alias pacman="pacman --color=auto"
 alias powerpill="powerpill --color=auto"
 
+alias i3config="nano ~/.config/i3/config"
+
 alias cmpt489="~/scripts/cmpt489connect"
 alias bbg="~/scripts/bbgConnect"
 alias bbgipt="~/scripts/bbgIpTablesSetup"
 
 alias calc="gnome-calculator"
 alias reboot="systemctl reboot"
+alias pingtest="ping 1.1.1.1"
 
 alias sl="sl -Fac"
 
-alias journalerrors="journalctl -f -n 1000 -p 1..3"
+alias journalerr="journalctl -f -n 250 -p 1..3"
 
 alias auraupdate="sudo aura -Akuax"
