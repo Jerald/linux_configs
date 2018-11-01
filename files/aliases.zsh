cf793964fae8c3fc03ecaa10f643da0177109f0d
Oscar <osmithsi@sfu.ca>
2018-11-01 13:35:28 -0700

    Update based on refinement of i3

--- files/aliases.zsh
+++ files/aliases.zsh
@@ -7,12 +7,16 @@ alias pacman="pacman --color=auto"
 alias powerpill="powerpill --color=auto"
 
 alias i3config="nano ~/.config/i3/config"
+alias xlog0="nano ~/.local/share/xorg/Xorg.0.log"
+alias xlog1="nano ~/.local/share/xorg/Xorg.1.log"
+
+alias i3lock="i3lock -fo"
 
 alias cmpt489="~/scripts/cmpt489connect"
 alias bbg="~/scripts/bbgConnect"
 alias bbgipt="~/scripts/bbgIpTablesSetup"
 
-alias calc="gnome-calculator"
+alias calc="calc"
 alias reboot="systemctl reboot"
 alias pingtest="ping 1.1.1.1"
 
